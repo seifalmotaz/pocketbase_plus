4b3cb9776e5fc826a8203a1285bb7175691d3ec2
Seif Almotaz <seifalmotaz@outlook.com>
2024-09-05 05:04:50 +0300

    example folder for pub.dev

--- example/lib/example.dart
+++ example/lib/example.dart
@@ -1,3 +1,12 @@
-int calculate() {
-  return 6 * 7;
+import 'package:example/models/matches.dart';
+import 'package:pocketbase/pocketbase.dart';
+
+Future<void> main(List<String> arguments) async {
+  final pb = PocketBase('http://exmaple.com');
+
+  final data = await pb.collection('match_requests').getFullList();
+
+  final matches = data.map((e) => MatchesModel.fromModel(e));
+  print(matches.first.id);
+  print(matches.first.created);
 }
